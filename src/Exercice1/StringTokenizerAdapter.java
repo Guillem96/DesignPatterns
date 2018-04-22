b1e06c71c5b490b0090ea71c165f4b9f627adc30
Guillem96 <guillem.orellana@gmail.com>
2018-04-22 11:17:46 +0200

    a)

---
+++ src/Exercice1/StringTokenizerAdapter.java
@@ -0,0 +1,32 @@
+package Exercice1;
+
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class StringTokenizerAdapter extends StringTokenizer implements Iterator {
+    public StringTokenizerAdapter(String s, String s1, boolean b) {
+        super(s, s1, b);
+    }
+
+    public StringTokenizerAdapter(String s, String s1) {
+        super(s, s1);
+    }
+
+    public StringTokenizerAdapter(String s) {
+        super(s);
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        // hasMoreTokens is the same but hasMoreElements
+        // is the function that Enumeration interface defines
+        return super.hasMoreElements();
+    }
+
+    @Override
+    public Object next() {
+        // Same as nextToken
+        return super.nextElement();
+    }
+}
