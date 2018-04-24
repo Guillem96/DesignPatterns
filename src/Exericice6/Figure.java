f26440e57e78c3ea9441bd75cee74804dc249827
Guillem96 <guillem.orellana@gmail.com>
2018-04-24 09:15:57 +0200

    Update Figure.java

--- src/Exericice6/Figure.java
+++ src/Exericice6/Figure.java
@@ -1,8 +1,8 @@
 package Exericice6;
 
 public abstract class Figure {
-    private float x;
-    private float y;
+    protected float x;
+    protected float y;
 
     Figure(float x, float y) {
         this.x = x;
